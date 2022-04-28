50585de371a83081cd30ca0c7cadc74bdabc5535
WoodenBox12 <ollysmith557@gmail.com>
2022-04-28 21:38:57 +0100

    class made

--- game.py
+++ game.py
@@ -1,22 +1,100 @@
 from random import randint
+from os import system, name
 from time import sleep
 
+def clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+# characters
+
+class knight:
+
+    health = 100;
+
+    def dificultyModifier(self, baseValue, dificulty, increase):
+        print("work1")
+        if increase == True :
+            
+            if dificulty == 1 :
+                return baseValue
+
+            elif dificulty == 5 :
+                return int(round(baseValue * 1.4, 0))
+
+            elif dificulty == 4 :
+                return int(round(baseValue * 1.3, 0))
+
+            elif dificulty == 3 :
+                return int(round(baseValue * 1.2, 0))
+
+            elif dificulty == 2 :
+                return int(round(baseValue * 1.1, 0))
+
+        elif increase == False :
+            print("work2")
+            if dificulty == 1 :
+                return baseValue
+
+            elif dificulty == 5 :
+                return int(round(baseValue * 0.6, 0))
+
+            elif dificulty == 4 :
+                return int(round(baseValue * 0.7, 0))
+
+            elif dificulty == 3 :
+                return int(round(baseValue * 0.8, 0))
+
+            elif dificulty == 2 :
+                return int(round(baseValue * 0.9, 0))
+
+        else:
+            return baseValue
+
+    def __init__(self):
+
+        self.health = self.dificultyModifier(self.health, dificulty, False)
+
+    
+
 # dificulty select
-dificulty = input("select dificulty between 1 and 100 or random ")
+
+dificulty = input("select dificulty between 1 and 5 or random: ")
 
 if dificulty.upper() == "RANDOM":
 
-    dificulty = randint(1,100)
+    dificulty = randint(1,5)
 
 else:
     try:
 
-        dificulty = int(dificulty)
+        if type(int(dificulty)) == int:
+    
+            if 1 <= int(dificulty) <= 5:
+
+                dificulty = int(dificulty)
+
+            else:
+
+                dificulty = randint(1,5)
 
     except:
 
-        dificulty = randint(1,100)
+        dificulty = randint(1,5)
+
+sleep(2)
+clear()
 
 # choose starter weapon
 
-starterWeapon = input("select starting weapon\n1 for dagger")
+starterWeapon = input("select player class\n1 for knight\n2 for archer\n")
+
+character = knight()
+
+print(character.health)
