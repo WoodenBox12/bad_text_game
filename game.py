7b978c69d4d879f8970bbbafc6c1f25cf27c8ed1
WoodenBox12 <ollysmith557@gmail.com>
2022-05-09 19:30:30 +0100

    changes

--- game.py
+++ game.py
@@ -179,6 +179,9 @@ main weapon is {self.mainWeapon[0]}
                 if choice == name:
                     
                     if self.currentHealth == self.maxHealth:
+
+                        print("already on max health")
+                        sleep(2)
                         break
 
                     elif self.currentHealth + self.inventory[i][2] > self.maxHealth:
@@ -233,7 +236,7 @@ class enemys:
 
             return " "
 
-    def __init__(self, health, damage, defence, startRange=None):
+    def __init__(self, health, damage, defence, startRange):
         
         self.distance = startRange
 
@@ -410,94 +413,176 @@ while True:
 
     elif choice.lower() == "melee":
 
-        if grandmother.distance == 0:
+        if "melee" in player.mainWeapon[1]:
 
-            damage = player.mainWeapon[2]
-            crit = False
+            if grandmother.distance == 0:
 
-            if randint(1,100) <= player.mainWeapon[3]:
+                damage = player.mainWeapon[2]
+                crit = False
 
-                damage *= round(player.mainWeapon[4])
-                crit = True
+                if randint(1,100) <= player.mainWeapon[3]:
 
-            print(f"{damage} damage dealt", end="    ")
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
 
-            if crit:
-                print("Critical hit!")
+                print(f"{damage} damage dealt", end="    ")
 
-            grandmother.currentHealth -= damage
+                if crit:
+                    print("Critical hit!")
 
-            print()
-            sleep(2)
+                grandmother.currentHealth -= damage
 
-        elif grandmother.distance == 1:
+                print()
+                sleep(2)
 
-            damage = round(player.mainWeapon[2] * (randint(8, 12) / 10))
-            crit = False
+            elif grandmother.distance == 1:
 
-            if randint(1,100) <= player.mainWeapon[3]:
+                damage = round(player.mainWeapon[2] * (randint(8, 12) / 10))
+                crit = False
 
-                damage *= round(player.mainWeapon[4])
-                crit = True
+                if randint(1,100) <= player.mainWeapon[3]:
 
-            print(f"{damage} damage dealt", end="    ")
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
 
-            if crit:
-                print("Critical hit!")
+                print(f"{damage} damage dealt", end="    ")
 
-            grandmother.currentHealth -= damage
+                if crit:
+                    print("Critical hit!")
 
-            print()
-            sleep(2)
+                grandmother.currentHealth -= damage
+
+                print()
+                sleep(2)
+
+            else:
+
+                print("too far away")
+                sleep(2)
 
         else:
-
-            print("too far away")
+            print("melee weapon not selected")
             sleep(2)
 
     elif choice.lower() == "ranged":
 
-        if grandmother.distance == 1:
+        if "ranged" in player.mainWeapon[1]:
 
-            damage = player.mainWeapon[2]
-            crit = False
+            if grandmother.distance == 1:
 
-            if randint(1,100) <= player.mainWeapon[3]:
+                damage = player.mainWeapon[2]
+                crit = False
 
-                damage *= round(player.mainWeapon[4])
-                crit = True
+                if randint(1,100) <= player.mainWeapon[3]:
 
-            print(f"{damage} damage dealt", end="    ")
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
 
-            if crit:
-                print("Critical hit!")
+                print(f"{damage} damage dealt", end="    ")
 
-            grandmother.currentHealth -= damage
+                if crit:
+                    print("Critical hit!")
 
-            print()
-            sleep(2)
+                grandmother.currentHealth -= damage
 
-        elif grandmother.distance == 2:
+                print()
+                sleep(2)
 
-            damage = round(player.mainWeapon[2] * (randint(8, 12) / 10))
-            crit = False
+            elif grandmother.distance == 2:
 
-            if randint(1,100) <= player.mainWeapon[3]:
+                damage = round(player.mainWeapon[2] * (randint(8, 12) / 10))
+                crit = False
 
-                damage *= round(player.mainWeapon[4])
-                crit = True
+                if randint(1,100) <= player.mainWeapon[3]:
 
-            print(f"{damage} damage dealt", end="    ")
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
 
-            if crit:
-                print("Critical hit!")
+                print(f"{damage} damage dealt", end="    ")
 
-            grandmother.currentHealth -= damage
+                if crit:
+                    print("Critical hit!")
 
-            print()
-            sleep(2)
+                grandmother.currentHealth -= damage
+
+                print()
+                sleep(2)
+
+            else:
+
+                print("too close")
+                sleep(2)
 
         else:
 
-            print("too close")
+            print("ranged weapon not selected")
+            sleep(2)
+
+    elif choice.lower() == "magic" and player.Type == " mage":
+
+        if "magic" in player.mainWeapon[1]:
+
+            if grandmother.distance == 0:
+
+                damage = player.mainWeapon[2]
+                crit = False
+
+                if randint(1,100) <= player.mainWeapon[3]:
+
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
+
+                print(f"{damage} damage dealt", end="    ")
+
+                if crit:
+                    print("Critical hit!")
+
+                grandmother.currentHealth -= damage
+
+                print()
+                sleep(2)
+
+            elif grandmother.distance == 1:
+
+                damage = round(player.mainWeapon[2] * (randint(8, 12) / 10))
+                crit = False
+
+                if randint(1,100) <= player.mainWeapon[3]:
+   
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
+
+                print(f"{damage} damage dealt", end="    ")
+
+                if crit:
+                    print("Critical hit!")
+
+                grandmother.currentHealth -= damage
+
+                print()
+                sleep(2)
+
+            elif grandmother.distance == 2:
+
+                damage = round(player.mainWeapon[2] * (randint(8, 12) / 10))
+                crit = False
+
+                if randint(1,100) <= player.mainWeapon[3]:
+   
+                    damage *= round(player.mainWeapon[4])
+                    crit = True
+
+                print(f"{damage} damage dealt", end="    ")
+
+                if crit:
+                    print("Critical hit!")
+
+                grandmother.currentHealth -= damage
+
+                print()
+                sleep(2)
+
+        else:
+
+            print("ranged weapon not selected")
             sleep(2)
